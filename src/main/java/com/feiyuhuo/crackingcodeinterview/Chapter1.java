24eb4a8f2914b3ec6fe68b05c7dafb314b2d276d
yanqiang.wyq <yanqiang.wyq@alibaba-inc.com>
2016-11-16 11:21:58 +0800

    add first algorithm

--- src/main/java/com/feiyuhuo/crackingcodeinterview/Chapter1.java
+++ src/main/java/com/feiyuhuo/crackingcodeinterview/Chapter1.java
@@ -5,8 +5,21 @@ package com.feiyuhuo.crackingcodeinterview;
  */
 public class Chapter1 {
 
+    public static boolean isUniqChars2(String str) {
+        boolean[] char_set = new boolean[256];
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i);
+            if (char_set[val]) {
+                return false;
+            }
+            char_set[val] = true;
+        }
+        return true;
+    }
+
+
 
     public static void main(String[] args) {
-
+        System.out.println(isUniqChars2("abc"));
     }
 }
